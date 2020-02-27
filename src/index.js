bc0dca60c0d58793cd1451cb3d94ccad6311ef8d
Vlad Bazhanov <vb@compliancetech.ru>
2020-02-27 16:25:40 +0300

    useState for login page

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter as Router } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
-import Routes from 'routes';
+import Routes from "routes";
+import TopBar from "components/topBar";
 
 const App = () => {
-    return (
-        <div>
-            <h3>Welcome to Hooks</h3>
-            <Router>
-                <Routes />
-            </Router>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Router>
+        <TopBar />
+        <Routes />
+      </Router>
+    </div>
+  );
+};
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
