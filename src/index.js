0c3b151344c65e48ccefe5f4ca17a40f000ceeca
Vlad Bazhanov <vb@compliancetech.ru>
2020-02-27 11:52:40 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+import Routes from 'routes';
+
+const App = () => {
+    return (
+        <div>
+            <h3>Welcome to Hooks</h3>
+            <Router>
+                <Routes />
+            </Router>
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
